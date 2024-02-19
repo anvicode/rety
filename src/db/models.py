dbc70a95555cfb7c3c7637e90d50cb81aee5edc7
Andrey Vorontsov <reygofor@gmail.com>
2024-02-19 13:27:33 +0300

    feat: add db models

--- src/db/models.py
+++ src/db/models.py
@@ -1,13 +1,7 @@
 from datetime import timezone
 
-from sqlalchemy import UUID, Column, DateTime, Float, String, Text, create_engine, func
+from sqlalchemy import UUID, Column, DateTime, Float, ForeignKey, String, Text, func
 from sqlalchemy.orm import declarative_base
-
-sqlite_database = "sqlite:///db_films.sqlite"
-
-
-engine = create_engine(sqlite_database)
-
 
 Base = declarative_base()
 
@@ -17,24 +11,45 @@ class FilmWork(Base):
 
     id = Column(UUID, primary_key=True)
     title = Column(String(50), nullable=False)
-    description = Column(Text, nullable=True)
-    creation_date = Column(DateTime)
-    file_path = Column(Text, nullable=True)
     rating = Column(Float)
     type = Column(String, nullable=False)
     created_at = Column(DateTime, server_default=func.now(tz=timezone.utc))
     updated_at = Column(DateTime, onupdate=func.now(tz=timezone.utc))
 
 
-# class TodoItem(Base):
-#     __tablename__ = "todo_item"
+class Genre(Base):
+    __tablename__ = "genre"
 
-#     id = Column(UUID, primary_key=True)
-#     title = Column(String(50), nullable=False)
-#     list_id = Column(ForeignKey(TodoList.id), nullable=False)
-#     completed = Column(Boolean, default=False)
-#     created_at = Column(DateTime, server_default=func.now(tz=timezone.utc))
+    id = Column(UUID, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(Text, nullable=True)
+    created_at = Column(DateTime, server_default=func.now(tz=timezone.utc))
+    updated_at = Column(DateTime, onupdate=func.now(tz=timezone.utc))
 
-Base.metadata.create_all(engine)
 
-print("Database tables created")
+class Person(Base):
+    __tablename__ = "person"
+
+    id = Column(UUID, primary_key=True)
+    full_name = Column(String(50), nullable=False)
+    created_at = Column(DateTime, server_default=func.now(tz=timezone.utc))
+    updated_at = Column(DateTime, onupdate=func.now(tz=timezone.utc))
+
+
+class GenreFilmWork(Base):
+    __tablename__ = "genre_film_work"
+
+    id = Column(UUID, primary_key=True)
+    film_work_id = Column(ForeignKey(FilmWork.id), nullable=False)
+    genre_id = Column(ForeignKey(Genre.id), nullable=False)
+    created_at = Column(DateTime, server_default=func.now(tz=timezone.utc))
+
+
+class PersonFilmWork(Base):
+    __tablename__ = "person_film_work"
+
+    id = Column(UUID, primary_key=True)
+    film_work_id = Column(ForeignKey(FilmWork.id), nullable=False)
+    person_id = Column(ForeignKey(Person.id), nullable=False)
+    role = Column(String(50), nullable=False)
+    created_at = Column(DateTime, server_default=func.now(tz=timezone.utc))
